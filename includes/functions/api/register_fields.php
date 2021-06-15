98ca0e80376fa145395754d8037e5426d443c3d7
Kevin Oliveira <me@kevinoliveira.com.br>
2021-06-14 23:39:25 -0300

    feat(api): Add custom param to album post type

--- includes/functions/api/register_fields.php
+++ includes/functions/api/register_fields.php
@@ -52,3 +52,24 @@ function get_rest_genres( $object, $field_name, $request ) {
     $out = $genres;
     return $out;
 }
+
+/**
+ * Plugin Name: REST API - Post list randomize
+ * Description: Randomize the content list in REST API passing `orderby=rand` as parameter.
+ * Version:     1.0.0
+ * Author:      Felipe Elia | Codeable
+ * Author URI:  https://codeable.io/developers/felipe-elia?ref=qGTOJ
+ */
+ 
+/**
+ * Add `rand` as an option for orderby param in REST API.
+ * Hook to `rest_{$this->post_type}_collection_params` filter.
+ *
+ * @param array $query_params Accepted parameters.
+ * @return array
+ */
+function add_rand_orderby_rest_post_collection_params( $query_params ) {
+	$query_params['orderby']['enum'][] = 'rand';
+	return $query_params;
+}
+add_filter( 'rest_album_collection_params', 'add_rand_orderby_rest_post_collection_params' );
