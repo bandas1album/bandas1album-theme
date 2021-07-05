e3a631d8ed6e6d5e4b71672b843b1a376ba2a290
Kevin Oliveira <me@kevinoliveira.com.br>
2021-07-05 19:18:26 -0300

    fix(api): Ajustando allow origin para todas as rotas

--- includes/functions/api/cors.php
+++ includes/functions/api/cors.php
@@ -4,7 +4,7 @@ add_action('init', 'handle_preflight');
 
 function handle_preflight() {
     $origin = get_http_origin();
-    header("Access-Control-Allow-Origin: localhost");
+    header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
     header("Access-Control-Allow-Credentials: true");
     header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
