dbbe36d5c6af3289221979f373edca2dab9e6b49
Kevin Oliveira <me@kevinoliveira.com.br>
2021-07-05 19:06:06 -0300

    fix(api): Removendo autenticação da api

--- includes/functions/api/cors.php
+++ includes/functions/api/cors.php
@@ -4,24 +4,13 @@ add_action('init', 'handle_preflight');
 
 function handle_preflight() {
     $origin = get_http_origin();
-    if ($origin === 'https://bandas1album.com.br') {
-        header("Access-Control-Allow-Origin: https://bandas1album.com.br");
-        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
-        header("Access-Control-Allow-Credentials: true");
-        header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    header("Access-Control-Allow-Origin: localhost");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-        if ('OPTIONS' == $_SERVER['REQUEST_METHOD']) {
-            status_header(200);
-            exit();
-        }
+    if ('OPTIONS' == $_SERVER['REQUEST_METHOD']) {
+        status_header(200);
+        exit();
     }
-}
-add_filter('rest_authentication_errors', 'rest_filter_incoming_connections');
-function rest_filter_incoming_connections($errors) {
-    $request_server = $_SERVER['REMOTE_ADDR'];
-    $origin = get_http_origin();
-    if ($origin !== 'https://bandas1album.com.br') return new WP_Error('forbidden_access', $origin, array(
-        'status' => 403
-    ));
-    return $errors;
 }
