eb44342552496c8ecb68fa7a8a6bbeafc139cd0b
Kevin Oliveira <me@kevinoliveira.com.br>
2021-07-05 18:46:54 -0300

    fix(api): Ajustando função para habilitar cors

--- includes/functions/api/cors.php
+++ includes/functions/api/cors.php
@@ -1,17 +1,27 @@
 <?php
 
-function initCors( $value ) {
-  $origin_url = '*';
+add_action('init', 'handle_preflight');
 
-  header( 'Access-Control-Allow-Origin: ' . $origin_url );
-  header( 'Access-Control-Allow-Methods: GET' );
-  header( 'Access-Control-Allow-Credentials: true' );
-  return $value;
+function handle_preflight() {
+    $origin = get_http_origin();
+    if ($origin === 'https://bandas1album.com.br') {
+        header("Access-Control-Allow-Origin: " . HEADLESS_FRONTEND_URL);
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Credentials: true");
+        header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if ('OPTIONS' == $_SERVER['REQUEST_METHOD']) {
+            status_header(200);
+            exit();
+        }
+    }
 }
-
-add_action( 'rest_api_init', function() {
-
-	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
-
-	add_filter( 'rest_pre_serve_request', 'initCors');
-}, 15 );
+add_filter('rest_authentication_errors', 'rest_filter_incoming_connections');
+function rest_filter_incoming_connections($errors) {
+    $request_server = $_SERVER['REMOTE_ADDR'];
+    $origin = get_http_origin();
+    if ($origin !== 'https://bandas1album.com.br') return new WP_Error('forbidden_access', $origin, array(
+        'status' => 403
+    ));
+    return $errors;
+}
