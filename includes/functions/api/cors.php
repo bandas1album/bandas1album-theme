586b1d3fa69b341b6b3920d6424a84a965d64444
Kevin Oliveira <me@kevinoliveira.com.br>
2021-07-05 18:52:59 -0300

    fix(api): Ajustando allow origin url

--- includes/functions/api/cors.php
+++ includes/functions/api/cors.php
@@ -5,7 +5,7 @@ add_action('init', 'handle_preflight');
 function handle_preflight() {
     $origin = get_http_origin();
     if ($origin === 'https://bandas1album.com.br') {
-        header("Access-Control-Allow-Origin: bandas1album.com.br");
+        header("Access-Control-Allow-Origin: https://bandas1album.com.br");
         header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
         header("Access-Control-Allow-Credentials: true");
         header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
