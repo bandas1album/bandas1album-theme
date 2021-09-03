2d2d924b8d1bfb7301657b171ab589a78070661d
Kevin Oliveira <me@kevinoliveira.com.br>
2021-09-02 23:01:49 -0300

    Add graphql filter to total posts

--- functions.php
+++ functions.php
@@ -7,5 +7,8 @@ include('includes/functions/api/custom_endpoints.php');
 // Post Types
 include('includes/functions/post-types.php');
 
+// Filters
+include('includes/functions/filters.php');
+
 // Images
 include('includes/functions/images.php');
